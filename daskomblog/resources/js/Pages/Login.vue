c6eb7522b4b3017f5089b8d8771676bdd6f45f08
alifsiregar <alifsiregar21@gmail.com>
2020-04-28 13:43:42 +0700

    add image and assistant code beside logout button

--- daskomblog/resources/js/Pages/Login.vue
+++ daskomblog/resources/js/Pages/Login.vue
@@ -43,6 +43,9 @@
                         - Fix UI for login
                         - Add logout UI -->
 
+                        <!-- user: TST
+                        pass: test123 -->
+
                         <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" @keyup.enter="login">
                             <div class="mb-4">
                                 <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
